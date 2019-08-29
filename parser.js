37456566e119ae0247983d7af763f84bf05da5b4
Roman <rk@pangnote.com>
2019-08-29 15:19:16 +0300

    -now windows1251 can be displayed updated parser.js

--- parser.js
+++ parser.js
@@ -1,10 +1,11 @@
-const http = require('http'),
-	  https = require('https'),
-	  request = require('request'),
-	  url = require('url'),
-	  config = require('config'),
-	  UserAgent = require('user-agents');
-	  require('events').EventEmitter.defaultMaxListeners = 15;
+const request = require('request'),
+	http = require('http'),
+	url = require('url'),
+	iconv  = require('iconv-lite'),
+	config = require('config'),
+	UserAgent = require('user-agents'),
+	html2json = require('html2json').html2json;
+require('events').EventEmitter.defaultMaxListeners = 15;
 
 
 export default class Parser {
@@ -15,31 +16,57 @@ export default class Parser {
 	async _parse(link) {
 		console.log('*** start parsing ***\n')
 		if(!link) link = config.get('host');
-		
+
 		const userAgent = new UserAgent();
-		let options = url.parse(link)
-			options.uri = link 
-			options.headers = {'User-Agent': userAgent.toString()} 
-		
+		let options = {
+			uri: url.parse(link), 
+			port : 80,
+			headers : {
+				'User-Agent': userAgent.toString(),
+				//	'Accept-Charset': "WINDOWS-1251",
+				//	'Content-Type': 'application/html; charset=windows-1251',
+			},
+			encoding: null,
+			jar: true
+		}
+		console.log('options=>', options)	
 		// Make a request
-		const req = await new Promise((resolve, reject) => {
-			request.get(options,{jar:true}, (err, res, body) => {
+		let data = []
+		let buffer = await new Promise(resolve => {
+			request(options, function(error, response, body) {
 				resolve(body)
 			});
 		})
-		return req;
+		var utf8Data = iconv.decode(new Buffer(buffer), "windows-1251");
+		console.log('taked data again=>', utf8Data);
+		let json = html2json(utf8Data)
+	//	let test = JSON.parse(json)
+		console.log('json', json)
+		return json;
 	}
 
 	async _parse_department() {
 		console.log('*** parsing departments *** \n')
 		let body = await this._parse();
-		console.log('body=>', body)
-		body = body.split(/\<\/.+\>/);
-		console.log('new body =>', body)
-//		return req;
-		
-	
+		//console.log('body=>', body)
+		//body = body.split(/\<\/.+\>/);
+		//console.log('new body =>', body)
+		/*let department = body.filter( i => i.match(/(valign="bottom")/))
+		//console.log('code of \n', ">".charCodeAt(0))
+		//department.map( i =>)
+		console.log('department', department)
+		console.log("after", this._get_rus(department))
+		//		return req;
+*/
 	}
+
+	_get_rus(data){
+		return data.map(d => d.split("").filter( i =>{
+			console.log('i chatcode', i.charCodeAt(0));
+			return i.charCodeAt(0) >= 1140 && i.charCodeAt(0)<= 1111 }
+		).join(""))
+	}
+
 }
 let P = new Parser();
 P._parse_department()
